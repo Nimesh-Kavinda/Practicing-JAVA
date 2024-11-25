e1143b6bb3417ae49128316100bca3bdcbd0eed5
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-25 23:41:54 +0530

    Learned that "extends" = "Is a" as a concept.

--- src/Student.java
+++ src/Student.java
@@ -9,9 +9,21 @@ public class Student extends Person {
         super(name , surname , age);
         // go and call constructor of super class
         super.getName();
+        // go and catch the getName method in super class
+
+        // inheritance means "IS a" connection
+
     }
 
     // Person is our parent class or supper class or our base class
     // Studens or Teacher is our child classes
+
+    public static void run(){
+        System.out.println("this student is running");
+    }
+
+    public String read(String bookname){
+        return bookname;
+    }
 
 }
