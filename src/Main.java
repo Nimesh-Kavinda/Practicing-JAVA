024907d584268e02c782c3aac103111cd86fd351
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-25 16:04:53 +0530

    Finish the Bancking app and complete first part of OOP concetps

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        Account account1 = new Account("Nimesh", "Kavinda", "nimesh@gmail.com", 10000 , 20012150, "0778525115");
 
+        System.out.println(account1.getName());
+
+        account1.putMoney(2000);
+        account1.takeMoney(5000);
     }
 
 
