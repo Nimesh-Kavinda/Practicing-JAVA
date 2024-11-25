e93d9ec5b5c0ff652d45c7ad9824bb220663b3d5
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-25 11:11:12 +0530

    Learned Teory about the OOP

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    // classes is like fabrics, and we use them to create objects
+    // In Rs. Class 500 is an object
+
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
