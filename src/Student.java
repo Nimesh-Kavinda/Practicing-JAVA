8d10b2ff996d7b7b61e4bbfe4f42e5f2c3194d46
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-25 15:05:59 +0530

    Create Getter and Setter Automatically and Learned about them Encapsulation

--- src/Student.java
+++ src/Student.java
@@ -3,26 +3,48 @@ public class Student {
 
         // Set & Get methods
 
-
-
     private String name;
     private String surname;
     private String email;
     private int age;
 
-    public void setName(String name){
+    public Student() {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
-     public void setSurname(String surname){
-        this.surname = surname;
-     }
-     public String getName() {
-        return name;
-     }
-     public String getSurname(){
+
+    public String getSurname() {
         return surname;
-     }
+    }
 
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
