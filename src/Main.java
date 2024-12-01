25b145cbd19eac915073f839d88c42c5351c156a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-12-01 09:16:49 +0530

    Start to learn java compositions

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Programmer programmer = new Programmer("Nimesh", "Kavinda",23,"java");
-        programmer.showInfo();
-        programmer.work();
-        CEO ceo = new CEO("Dimuth", "Adithya",30,12);
-        ceo.showInfo();
-        Driver driver = new Driver("Dulanka", "Nimshara", 25,3);
-        driver.showInfo();
+
     }
 
 
