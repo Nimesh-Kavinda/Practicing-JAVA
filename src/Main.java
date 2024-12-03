c5ee71de6c47b9fe726765acd0e1f1f95ccbaa5a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-12-04 00:33:29 +0530

    Start to Learn Java Wrapper Classes

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-    // instanceof is use to compare
-
-        Person person = new Person();
-        Student student = new Student();
-        Teacher teacher = new Teacher();
-
-        System.out.println(teacher instanceof Person);
 
     }
 }
