01c99c9378628409ea855b94470d18cb09e65b57
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-12-04 00:18:46 +0530

    Repeat OOP that learned so far

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
     public static void main(String[] args) {
     // what we can do using oop
 
+        // Class
+        // Object
+        // Inheritance
+        // .java
+
     }
 }
 
