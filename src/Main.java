f19da7264dfb516479d585105e2d10efcab2c2f9
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-12-04 00:49:02 +0530

    You need to Wrapper Classes to Use methods

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,32 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        int first = 5; // Integer
+        double second = 6.6; // Doubel
+        char c = 'C'; // Charcter
+        long l = 189; // Long
+        float f = 66; // Float
+        short s = 55; // Short
+        byte b = 1; // Byte
+        boolean bl = true; // Boolean
+
+        Integer integer;
+        Double d;
+        Character cc;
+        Boolean bb;
+        Long ll;
+
+
+        Character nn = 'b';
+        char cha = Character.toUpperCase(nn);
+        System.out.println(cha);
+        String ss = "addiads";
+        Integer first2 = 5;
+        Integer fff = 7;
+
+
+
     }
 }
 
